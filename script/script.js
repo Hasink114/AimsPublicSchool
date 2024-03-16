bfbcb8ee695a86839d3c12df135b87f0719122ac
Hasink114 <111485995+Hasink114@users.noreply.github.com>
2024-03-16 12:51:58 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,13 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('.ctaa');
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            alert('Files will be uploaded after 1:00 pm.');
-        });
-    });
-});
-*/
 
 const sr = ScrollReveal ({
     distance: '65px',
